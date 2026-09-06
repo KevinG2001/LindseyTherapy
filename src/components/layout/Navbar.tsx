c5c843315ca46872c20b1bcc9cc154be41461d1b
Kevin G <kevinglennon01@gmail.com>
2026-09-06 11:51:11 +0100

    Navbar layout

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,23 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+const pages = ["Home", "About", "Contact"];
+
 function Navbar() {
 	return (
-		<div>
-			<div>Navbar</div>
-		</div>
+		<AppBar position="static">
+			<Toolbar>
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					Lindsey Psycho Therapy
+				</Typography>
+				<Box sx={{ display: { xs: "none", md: "flex" } }}>
+					{pages.map((page) => (
+						<Typography key={page} variant="button" sx={{ mx: 2 }}>
+							{page}
+						</Typography>
+					))}
+				</Box>
+			</Toolbar>
+		</AppBar>
 	);
 }
 
